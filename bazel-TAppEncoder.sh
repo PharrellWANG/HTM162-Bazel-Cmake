43ab5a337bd16e1cd30ca3cabff396cd76828c3d
Pharrell.zx <wzxnuaa@gmail.com>
2017-09-21 21:30:47 +0800

    .sh added

---
+++ bazel-TAppEncoder.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd
+cd tensorflow
+bazel build //HTM162/App/TAppEncoder/...
+cd
+cd tensorflow/HTM162
---
+++ bazel-TLibCommon.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd
+cd tensorflow
+bazel build //HTM162/Lib/TLibCommon/...
+cd
+cd tensorflow/HTM162
